7ab5b58864c11991a2bf24cfef0e95af1aeb859e
Susan Belling <susan.belling@peachpayments.com>
2021-09-20 09:26:37 +0200

    updates

--- staticfiles/javascript/showitems.js
+++
@@ -1,8 +0,0 @@
-function changeColor(o) {
-	o.style.color=(o.style.color=='green')?('#007bff'):('green');
-	}
-
-function showItems(e,b) {
-	changeColor(e) ;
-	document.getElementById('d_'+b).style.display =(document.getElementById('d_'+b).style.display=='block')?('none'):('block');
-	}
--- staticfiles/javascript/toggledailytimes.js
+++
@@ -1,7 +0,0 @@
-function toggleDailyTimes() {
-    if ($('#id_bool_daily').prop('checked')) {
-        $('.open_close_block').hide()
-    } else {
-        $('.open_close_block').show()
-    }
-}
