75d55bf57531759f062ae53ed46cff2697de81b9
Susan Belling <susan.belling@peachpayments.com>
2021-10-02 16:42:30 +0200

    migration

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'holiday.planner.help@gmail.com'
-EMAIL_HOST_PASSWORD = 'zcvjuochcgoglhlt'
+EMAIL_HOST_PASSWORD = 'igktuwalagetotgn'
